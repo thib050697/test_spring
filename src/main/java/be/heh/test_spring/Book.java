40eec80f7cffebc74a30b7c2cd8db6c88c80051e
thib050697 <thibault.stevanoni@std.heh.be>
2017-10-17 11:45:55 +0200

    Initialized project

---
+++ src/main/java/be/heh/test_spring/Book.java
@@ -0,0 +1,50 @@
+package be.heh.test_spring;
+
+public class Book {
+
+    private Long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public void setReader(String reader) {
+        this.reader = reader;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
